d03179ad91bd56569aa2da75980c8ffcf7311e39
blinkspark <3829294+blinkspark@users.noreply.github.com>
2021-06-17 12:56:24 +0800

    add to peerinfos

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/blinkspark/go-util
 
 go 1.16
 
-require github.com/multiformats/go-multiaddr v0.3.2
+require (
+	github.com/libp2p/go-libp2p-core v0.8.5
+	github.com/multiformats/go-multiaddr v0.3.2
+)
--- libp2p.go
+++ libp2p.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"github.com/libp2p/go-libp2p-core/peer"
 	maddr "github.com/multiformats/go-multiaddr"
 )
 
@@ -13,4 +14,12 @@ func StringsToAddrs(addrStrings ...string) (maddrs []maddr.Multiaddr, err error)
 		maddrs = append(maddrs, addr)
 	}
 	return
+}
+
+func StringsToPeerInfos(addrStrings ...string) ([]peer.AddrInfo, error) {
+	maddrs, err := StringsToAddrs(addrStrings...)
+	if err != nil {
+		return nil, err
+	}
+	return peer.AddrInfosFromP2pAddrs(maddrs...)
 }
