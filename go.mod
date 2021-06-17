8a24ab1907cfad1adbcf97039f0095b2e1330ccd
blinkspark <3829294+blinkspark@users.noreply.github.com>
2021-06-17 12:32:34 +0800

    add libp2p util

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/blinkspark/go-util
+
+go 1.16
---
+++ libp2p.go
@@ -0,0 +1,12 @@
+package util
+
+func StringsToAddrs(addrStrings ...string) (maddrs []maddr.Multiaddr, err error) {
+	for _, addrString := range addrStrings {
+		addr, err := maddr.NewMultiaddr(addrString)
+		if err != nil {
+			return maddrs, err
+		}
+		maddrs = append(maddrs, addr)
+	}
+	return
+}
