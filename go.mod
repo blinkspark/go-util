d1030e65f6b8265e87adb1328d72f52d7792c8e7
blinkspark <3829294+blinkspark@users.noreply.github.com>
2021-06-17 12:37:19 +0800

    add import

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/blinkspark/go-util
 
 go 1.16
+
+require github.com/multiformats/go-multiaddr v0.3.2
--- libp2p.go
+++ libp2p.go
@@ -1,5 +1,9 @@
 package util
 
+import (
+	maddr "github.com/multiformats/go-multiaddr"
+)
+
 func StringsToAddrs(addrStrings ...string) (maddrs []maddr.Multiaddr, err error) {
 	for _, addrString := range addrStrings {
 		addr, err := maddr.NewMultiaddr(addrString)
